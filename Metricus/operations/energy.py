2b9cb3a2d6b658b6ba55fad752bf2ac6b0cceb03
Guilherme <guilhermefreschix@gmail.com>
2024-11-13 15:52:52 -0300

    minor changes in operations

--- Metricus/operations/energy.py
+++ Metricus/operations/energy.py
@@ -3,21 +3,21 @@ from typing import Union
 
 unit_list = ['electronvolt', 'calorie', 'joule', 'british_thermal_unit', 'kilocalorie', 'kilowatt_hour']
 
-def energy_converter(value: float, from_unit: str, to_unit: str, with_unit: bool = False) -> Union[float, str]:
+def energy_converter(energy: float, from_unit: str, to_unit: str, with_unit: bool = False) -> Union[float, str]:
     if from_unit not in unit_list or to_unit not in unit_list:
         raise ValueError("Unknown unit of measurement")
     else:
         if from_unit == 'electronvolt':
-            return ef.Electronvolt(value, with_unit=with_unit).electronvolt_to(to_unit)
+            return ef.Electronvolt(energy, with_unit=with_unit).electronvolt_to(to_unit)
         elif from_unit == 'calorie':
-            return ef.Calorie(value, with_unit=with_unit).calorie_to(to_unit)
+            return ef.Calorie(energy, with_unit=with_unit).calorie_to(to_unit)
         elif from_unit == 'joule':
-            return ef.Joule(value, with_unit=with_unit).joule_to(to_unit)
+            return ef.Joule(energy, with_unit=with_unit).joule_to(to_unit)
         elif from_unit == 'british_thermal_unit':
-            return ef.BritishThermalUnit(value, with_unit=with_unit).british_thermal_unit_to(to_unit)
+            return ef.BritishThermalUnit(energy, with_unit=with_unit).british_thermal_unit_to(to_unit)
         elif from_unit == 'kilocalorie':
-            return ef.Kilocalorie(value, with_unit=with_unit).kilocalorie_to(to_unit)
+            return ef.Kilocalorie(energy, with_unit=with_unit).kilocalorie_to(to_unit)
         elif from_unit == 'kilowatt_hour':
-            return ef.KilowattHour(value, with_unit=with_unit).kilowatt_hour_to(to_unit)
+            return ef.KilowattHour(energy, with_unit=with_unit).kilowatt_hour_to(to_unit)
         else:
             raise ValueError("Unknown unit of measurement")
--- Metricus/operations/mass.py
+++ Metricus/operations/mass.py
@@ -6,27 +6,27 @@ unit_list = [
     'pound', 'kilogram', 'stone', 'slug', 'tonne'
 ]
 
-def mass_converter(value: float, from_unit: str, to_unit: str, with_unit: bool = False) -> Union[float, str]:
+def mass_converter(mass: float, from_unit: str, to_unit: str, with_unit: bool = False) -> Union[float, str]:
     if from_unit not in unit_list or to_unit not in unit_list:
         raise ValueError("The measurement has an unknown unit")
     else:
         if from_unit == 'milligram':
-            return mf.Milligram(value, with_unit=with_unit).milligram_to(to_unit)
+            return mf.Milligram(mass, with_unit=with_unit).milligram_to(to_unit)
         elif from_unit == 'carat':
-            return mf.Carat(value, with_unit=with_unit).carat_to(to_unit)
+            return mf.Carat(mass, with_unit=with_unit).carat_to(to_unit)
         elif from_unit == 'gram':
-            return mf.Gram(value, with_unit=with_unit).gram_to(to_unit)
+            return mf.Gram(mass, with_unit=with_unit).gram_to(to_unit)
         elif from_unit == 'ounce':
-            return mf.Ounce(value, with_unit=with_unit).ounce_to(to_unit)
+            return mf.Ounce(mass, with_unit=with_unit).ounce_to(to_unit)
         elif from_unit == 'pound':
-            return mf.Pound(value, with_unit=with_unit).pound_to(to_unit)
+            return mf.Pound(mass, with_unit=with_unit).pound_to(to_unit)
         elif from_unit == 'kilogram':
-            return mf.Kilogram(value, with_unit=with_unit).kilogram_to(to_unit)
+            return mf.Kilogram(mass, with_unit=with_unit).kilogram_to(to_unit)
         elif from_unit == 'stone':
-            return mf.Stone(value, with_unit=with_unit).stone_to(to_unit)
+            return mf.Stone(mass, with_unit=with_unit).stone_to(to_unit)
         elif from_unit == 'slug':
-            return mf.Slug(value, with_unit=with_unit).slug_to(to_unit)
+            return mf.Slug(mass, with_unit=with_unit).slug_to(to_unit)
         elif from_unit == 'tonne':
-            return mf.Tonne(value, with_unit=with_unit).tonne_to(to_unit)
+            return mf.Tonne(mass, with_unit=with_unit).tonne_to(to_unit)
         else:
             raise ValueError("The measurement has an unknown unit")
