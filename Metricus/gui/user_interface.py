6cbb26896fd0f691da83823047db344d3fb0198c
Guilherme <guilhermefreschix@gmail.com>
2024-11-19 19:50:33 -0300

    minor update in user_interface

--- Metricus/gui/user_interface.py
+++ Metricus/gui/user_interface.py
@@ -1,12 +1,13 @@
 import tkinter as tk
 
-root = tk.Tk()
-root.geometry("400x300")
+def gui():
+  root = tk.Tk()
+  root.geometry("400x300")
 
-label = tk.Label(root, text="Testing GUI")
-label.grid(row=0, column=0, sticky="nsew")
+  label = tk.Label(root, text="Testing GUI")
+  label.grid(row=0, column=0, sticky="nsew")
 
-root.grid_rowconfigure(0, weight=1)
-root.grid_columnconfigure(0, weight=1)
+  root.grid_rowconfigure(0, weight=1)
+  root.grid_columnconfigure(0, weight=1)
 
-root.mainloop()
+  root.mainloop()
