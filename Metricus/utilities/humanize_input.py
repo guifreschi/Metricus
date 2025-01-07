c3f7d6e38eace9197ac8811dbe523a5987bec237
Guilherme Freschi <guilhermefreschix@gmail.com>
2025-01-07 19:31:53 -0300

    Fix issue with humanize input for specific units such as 'siemens' and 'celsius'

--- Metricus/utilities/humanize_input.py
+++ Metricus/utilities/humanize_input.py
@@ -3,9 +3,7 @@ def humanize_input(message: str) -> str:
     Transforms a given string into a standardized, machine-friendly format by converting it 
     to lowercase and replacing spaces with underscores. It also handles plural forms, 
     such as converting 'meters' to 'meter' and special cases like 'feet' to 'foot'.
-
-    This function is particularly useful for creating consistent identifiers, file names, 
-    or variable names from human-readable text.
+    Additional special handling for 'm/s', 'siemens', and 'celsius'.
 
     Parameters:
     message (str): The input string to be transformed.
@@ -16,30 +14,27 @@ def humanize_input(message: str) -> str:
          - Plural forms converted to singular (e.g., 'meters' -> 'meter').
          - Spaces replaced by underscores.
          - Special cases like 'feet' handled explicitly (e.g., 'feet' -> 'foot').
-
-    Examples:
-    >>> humanize_input('Meters per second squared')
-    'meter_per_second_squared'
-    
-    >>> humanize_input('Nautical Miles')
-    'nautical_mile'
-
-    >>> humanize_input('feet')
-    'foot'
-
-    >>> humanize_input('Glasses of water')
-    'glass_of_water'
     """
+    # Handle special cases
     if message.lower() == 'feet':
         message = 'foot'
+    elif message.lower() == 'siemens':
+        return 'siemens'  # No change for 'siemens'
+    elif message.lower() == 'celsius':
+        return'celsius'  # No change for 'celsius'
+    elif message.lower() == 'm/s':
+        return 'meter_per_second'  # Handle 'm/s' directly
     
+    # Normalize the message
     normalized = message.lower().strip()
 
+    # Split the message into words and handle plural forms
     words = normalized.split()
     normalized_words = [
         word[:-1] if word.endswith('s') and not word.endswith('ss') else word
         for word in words
     ]
 
+    # Join the words with underscores
     normalized = '_'.join(normalized_words)
     return normalized
--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r", encoding='utf-8') as arq:
 
 setup(
     name="metricus",
-    version="1.0.2",
+    version="1.0.3",
     packages=find_packages(),
     install_requires=["matplotlib"],
     long_description=readme,
